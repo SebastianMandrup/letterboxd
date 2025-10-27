f8af64b61ed2b2e42f069a88be33a6be33609fb2
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2025-10-27 08:48:57 +0100

    sp/fix/pages_import_missing

--- letterboxd-frontend/src/app.tsx
+++ letterboxd-frontend/src/app.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import IndexPage from './pages';
 import FilmsPage from './pages/Films';
+import IndexPage from './pages/Index';
 
 export default function App() {
     return (
