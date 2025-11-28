493a71dba2e918a0ee74fad0ce8a9387bd9f5c2f
SebastianMandrup <sebastianmandrup@outlook.com>
2025-11-28 16:20:05 +0100

    added fetchcookie dev dependency

--- letterboxd-frontend/tests/integration/user-auth.test.ts
+++ letterboxd-frontend/tests/integration/user-auth.test.ts
@@ -1,19 +1,23 @@
 import dotenv from 'dotenv';
 import { describe, it, expect } from 'vitest';
+import fetch from 'node-fetch';
+import fetchCookie from 'fetch-cookie';
 
 dotenv.config();
 
 const VITE_API_URL = process.env.VITE_API_URL;
 
+const fetchWithCookies = fetchCookie(fetch);
+
 describe('Auth API Integration', () => {
 
 	it('registers a user successfully', async () => {
-		const res = await fetch(`${VITE_API_URL}/users`, {
+		const res = await fetchWithCookies(`${VITE_API_URL}/users`, {
 			method: 'POST',
 			body: JSON.stringify({ username: 'test', password: 'password', email: 'test@example.com' }),
 			headers: { 'Content-Type': 'application/json' },
 		});
-		const data = await res.json();
+		const data: any = await res.json();
 
 		expect(res.status).toBe(201);
 		expect(data.message).toBe('User created successfully');
@@ -25,7 +29,7 @@ describe('Auth API Integration', () => {
 			body: JSON.stringify({ username: 'test', password: 'password' }),
 			headers: { 'Content-Type': 'application/json' },
 		});
-		const data = await res.json();
+		const data: any = await res.json();
 
 		console.log(data);
 
@@ -36,11 +40,11 @@ describe('Auth API Integration', () => {
 
 	it('logs the user out successfully', async () => {
 
-		const res = await fetch(`${VITE_API_URL}/auth/logout`, {
+		const res = await fetchWithCookies(`${VITE_API_URL}/auth/logout`, {
 			method: 'POST',
 		});
 
-		const data = await res.json();
+		const data: any = await res.json();
 
 		console.log(data);
 
