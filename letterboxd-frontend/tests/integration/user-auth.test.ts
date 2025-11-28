5d677b0a388b241a542cbe0646635a7642d36060
SebastianMandrup <sebastianmandrup@outlook.com>
2025-11-28 15:44:29 +0100

    same change in the right place

--- letterboxd-frontend/tests/integration/user-auth.test.ts
+++ letterboxd-frontend/tests/integration/user-auth.test.ts
@@ -8,28 +8,24 @@ const VITE_API_URL = process.env.VITE_API_URL;
 describe('Auth API Integration', () => {
 
 	it('registers a user successfully', async () => {
-		const res = await fetch(`${VITE_API_URL}/auth/register`, {
+		const res = await fetch(`${VITE_API_URL}/users`, {
 			method: 'POST',
 			body: JSON.stringify({ username: 'test', password: 'test', email: 'test@example.com' }),
 			headers: { 'Content-Type': 'application/json' },
 		});
 		const data = await res.json();
 
-		console.log('Registration Response:', data);
-
 		expect(res.status).toBe(201);
 		expect(data.user).toBeDefined();
 	});
 
 	it('logs in a user successfully', async () => {
-		const res = await fetch(`${VITE_API_URL}/users`, {
+		const res = await fetch(`${VITE_API_URL}/auth/login`, {
 			method: 'POST',
 			body: JSON.stringify({ username: 'test', password: 'test' }),
 			headers: { 'Content-Type': 'application/json' },
 		});
 		const data = await res.json();
-
-		console.log('Login Response:', data);
 
 		expect(res.status).toBe(200);
 		expect(data.user).toBeDefined();
